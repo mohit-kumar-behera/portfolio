c3c38e539d90c74c5fbdfee0b871ae1c91c9d5dd
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-08-25 02:54:50 +0530

    generate dynamic url for contact type selected

--- contact/admin.py
+++ contact/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from contact.models import Contact
+from contact.models import Contact, Address
 
 class ContactAdmin(admin.ModelAdmin):
-    readonly_fields = ('last_updated',)
+    readonly_fields = ('last_updated', 'url')
     list_display = ('profile', 'type', 'value')
     list_filter = ('profile', 'type')
     search_fields = ('profile', 'type')
     ordering = ('profile',)
 
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'type', 'city', 'state')
+    list_filter = ('type', 'city', 'district', 'state')
+    search_fields = ('profile', 'type', 'city', 'district', 'state')
+    ordering = ('state',)
+
 admin.site.register(Contact, ContactAdmin)
+admin.site.register(Address, AddressAdmin)
--- contact/models.py
+++ contact/models.py
@@ -7,11 +7,18 @@ CONTACT_TYPE = (
     ('email', 'email')
 )
 
+ADDRESS_CHOICE = (
+    ('work', 'work'),
+    ('home', 'home'),
+    ('other', 'other'),
+)
+
 class Contact(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
     profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
     type = models.CharField(verbose_name='Select type', max_length=10, choices=CONTACT_TYPE) 
-    value = models.CharField(verbose_name='Enter Contact', max_length=120)
+    value = models.CharField(verbose_name='Enter Contact', max_length=40)
+    url = models.CharField(max_length=50)
     last_updated = models.DateField(auto_now=True, null=True, blank=True)
 
     def __str__(self):
@@ -20,5 +27,33 @@ class Contact(models.Model):
     def __unicode__(self):
         return f'{self.profile} {self.value}'
     
+    def save(self, *args, **kwargs):
+        tag = 'tel' if self.type == 'phone' else 'mailto'
+        self.url = f'{tag}:{self.value}'
+        super(Contact, self).save(*args, **kwargs)
+        
+    
     class Meta:
         verbose_name_plural = 'Contact Detail'
+
+
+class Address(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+    type = models.CharField(verbose_name='Address Type', max_length=10, choices=ADDRESS_CHOICE)
+    street = models.CharField(verbose_name='Street', max_length=255)
+    city = models.CharField(verbose_name='City', max_length=40)
+    district = models.CharField(verbose_name='District', max_length=40)
+    pin = models.CharField(verbose_name='PIN', max_length=6)
+    state = models.CharField(verbose_name='State', max_length=40)
+    coutry = models.CharField(verbose_name='Country', max_length=40)
+    mapURL = models.URLField(verbose_name='URL of MAP', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.profile} {self.type} address'
+
+    def __unicode__(self):
+        return f'{self.profile} {self.type} address'
+    
+    class Meta:
+        verbose_name_plural = 'Address Detail'
