3467565301f3229d1b8428a0684105e239812eea
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-08-25 02:30:35 +0530

    add contact model

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from contact.models import Contact
 
-# Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    readonly_fields = ('last_updated',)
+    list_display = ('profile', 'type', 'value')
+    list_filter = ('profile', 'type')
+    search_fields = ('profile', 'type')
+    ordering = ('profile',)
+
+admin.site.register(Contact, ContactAdmin)
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from home.models import Profile
+import uuid
 
-# Create your models here.
+CONTACT_TYPE = (
+    ('phone', 'phone'),
+    ('email', 'email')
+)
+
+class Contact(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+    type = models.CharField(verbose_name='Select type', max_length=10, choices=CONTACT_TYPE) 
+    value = models.CharField(verbose_name='Enter Contact', max_length=120)
+    last_updated = models.DateField(auto_now=True, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.profile} {self.value}'
+
+    def __unicode__(self):
+        return f'{self.profile} {self.value}'
+    
+    class Meta:
+        verbose_name_plural = 'Contact Detail'
