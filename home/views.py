ead164ec3339a534c6d7c16b49d381e98ffea3a4
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-09-16 02:06:12 +0530

    render home page

--- home/views.py
+++ home/views.py
@@ -1,16 +1,8 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
-
-
+from django.shortcuts import render
 from home.user import get_user, get_profile
-from home.models import ProfileImage
 
 def home_view(request):
 	profile = get_profile(user=get_user())
-	profile_image = ProfileImage.objects.get(profile=profile)
-	context = {
-		'profile': profile,
-		'img': profile_image
-	}
+	context = {'profile': profile}
 	return render(request, 'home/home.html', context)
 
